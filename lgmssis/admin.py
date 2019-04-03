f608ac9d47543ccde91ee27431944446863c1a01
Chobe <42force@gmail.com>
2019-04-03 20:14:35 +0800

    corrected flatpage

--- lgmssis/admin.py
+++ lgmssis/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.flatpages.admin import FlatPageAdmin
-from django.contrib.flatpages.models import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
