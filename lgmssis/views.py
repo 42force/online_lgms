84b32f64d08a8651d8fd4407a0bf730875a58209
Chobe <42force@gmail.com>
2019-04-15 15:46:38 +0800

    response template example

--- lgmssis/views.py
+++ lgmssis/views.py
@@ -8,5 +8,17 @@ def index(request):
     return render(request, 'lgmssis/index.html')
 
 
+# def courselist(request):
+#     return render(request, 'lgmssis/course-listing.html')
+def my_render_callback(response):
+    # Do content-sensitive processing
+    do_post_processing()
+
+
 def courselist(request):
-    return render(request, 'lgmssis/course-listing.html')
+    # Create a response
+    response = TemplateResponse(request, 'lgmssis/course-listing.html', {})
+    # Register the callback
+    response.add_post_render_callback(my_render_callback)
+    # Return the response
+    return response
