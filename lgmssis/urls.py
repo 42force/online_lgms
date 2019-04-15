72fee13bfcae9101782eebcb5ee6b7a699e8ba9c
Chobe <42force@gmail.com>
2019-04-15 16:00:35 +0800

    c

--- lgmssis/urls.py
+++ lgmssis/urls.py
@@ -8,7 +8,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 
-    path('courselist/', CourseListView.as_view()),
+    path('course', CourseListView.as_view()),
 
     #path('hello/', MyView.as_view(), name='my-view'),
 
