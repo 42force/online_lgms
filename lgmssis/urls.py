88c9dbc0b549a33269fafd062788a7f35c6b8333
Chobe <42force@gmail.com>
2019-04-16 10:46:54 +0800

    revised

--- lgmssis/urls.py
+++ lgmssis/urls.py
@@ -2,13 +2,11 @@ from django.urls import path
 #from lgmssis.views import MyView
 from . import views
 
-app_name= 'lgmssis'
 
 urlpatterns = [
     path('', views.index, name='index'),
 
     path('login/', views.login, name='login')
-
 
     #path('hello/', MyView.as_view(), name='my-view'),
 
