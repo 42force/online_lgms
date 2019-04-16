d2edd075a4ef36e969921e3d0dd000dfbef1c30e
Chobe <42force@gmail.com>
2019-04-16 11:10:04 +0800

    ,

--- lgmssis/urls.py
+++ lgmssis/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
 
     path('accounts/', include('django.contrib.auth.urls')),
 
