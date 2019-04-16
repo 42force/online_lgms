a99ff550074ea8872ece3740190e04dbe1ae3b44
Chobe <42force@gmail.com>
2019-04-16 12:01:22 +0800

    static

--- online_lgms/settings.py
+++ online_lgms/settings.py
@@ -138,5 +138,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = '/home/lgms/lgms/online_lgms/staticfiles'
 STATIC_URL = '/static/'
