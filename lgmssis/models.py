357d4044f3da1bb75c564ced0bce551ccf734734
Chobe <42force@gmail.com>
2019-05-21 11:24:43 +0800

    changes in model removed the default function of get country

--- lgmssis/models.py
+++ lgmssis/models.py
@@ -7,26 +7,26 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class CountryOption(models.Model):
-    name = models.CharField(max_length=500)
-    def __unicode__(self):
-        return unicode(self.name)
+    name = models.CharField('Country Options', max_length=500)
+
+    def __str__(self):
+        return '%s' % (self.name)
+    
     class Meta:
-        ordering = ['name']
+        verbose_name_plural = 'Country Options List'
 
 
-def get_default_country():
-    return CountryOption.objects.get_or_create(name=settings.ADMISSIONS_DEFAULT_COUNTRY)
 
 
-# class Applicant(models.Model):
-
-#     fname = models.CharField(max_length=255, verbose_name="First Name")
-#     lname = models.CharField(max_length=255, verbose_name="Last Name")
-#     streetname = models.CharField(max_length=255, verbose_name="Street Name")
-#     cityname = models.CharField(max_length=255, verbose_name="City Name")
-#     zip = models.IntegerField('Zip Code', blank=True, null=Trues)
-#     country_of_birth = models.ForeignKey(CountryOption, blank=True, null=True, default=get_default_country, on_delete=models.CASCADE)
-#     mobilenumber = PhoneNumberField('Mobile Number',help_text='MOBILE FORMAT : +639178888888', blank=True)
+class Applicant(models.Model):
+    pass
+    # fname = models.CharField(max_length=255, verbose_name="First Name")
+    # lname = models.CharField(max_length=255, verbose_name="Last Name")
+    # streetname = models.CharField(max_length=255, verbose_name="Street Name")
+    # cityname = models.CharField(max_length=255, verbose_name="City Name")
+    # zip = models.IntegerField('Zip Code', blank=True, null=True)
+    # country_of_birth = models.ForeignKey(CountryOption, blank=True, null=True, on_delete=models.CASCADE)
+    # mobilenumber = PhoneNumberField('Mobile Number',help_text='MOBILE FORMAT : +639178888888', blank=True)
  
     
 
