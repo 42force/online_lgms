b1b342838d0ac0eb17d2ffa9c8b394e76e1c28b7
Chobe <42force@gmail.com>
2019-06-17 11:04:58 +0800

    added apply

--- lgmsgrades/admin.py
+++ lgmsgrades/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Grade
 
 # Register your models here.
+
+class GradeCommentAdmin(admin.ModelAdmin):
+    pass
 class GradeAdmin(admin.ModelAdmin):
     list_display = ['grade', 'course', 'student', 'marking_period', 'override_final']
     list_filter = ['date', 'override_final']
--- lgmsgrades/views.py
+++ lgmsgrades/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.contenttypes.models import ContentType
+from django.contrib import messages
+from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
+from django.conf import settings
 
+from django.urls import reverse
+
+#from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.db.models import Q
+
+
+
+from django import forms
+
+
+import datetime
+import time
+import logging
 # Create your views here.
+
